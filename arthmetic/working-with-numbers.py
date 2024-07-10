972a1b5d8a9b15a6464819fbc4b7a1a6254b2421
helykish <helykish@gmail.com>
2024-07-10 03:38:18 +0300

    new files added

---
+++ arthmetic/working-with-numbers.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+print("1 day is equal to 24 hours")
+print("One day is equal to " + str(60*60*24) + " Seconds")
+print(f"20 days is equal to {20*20*24} hours") #new_edition
+print("")
+
+#USING VARIABLE
+calc_hours = 24 * 60
+unit = "minutes"
+print(f"20 days is equal to {20 * calc_hours} {unit}")
+print(f"30 days is equal to {30 * calc_hours} {unit}")
---
+++ test1.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+print("My first program")
+price = 100
+qty = 5
+total = price*qty
+print ("Total = ", total)
+
+
+
+name = input("Enter your name : ")
+city = input("Enter your city : ")
+
+print ("Hello My name is", name)
+print ("I am from ", city)
+
